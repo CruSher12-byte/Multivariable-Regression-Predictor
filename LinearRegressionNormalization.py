5e9725f63585c7e8293b76524f943714f746c50d
Rishi More <rishimore102@gmail.com>
2022-06-07 05:52:28 +0530

    Updated

--- LinearRegressionNormalization.py
+++ LinearRegressionNormalization.py
@@ -1,91 +1,58 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+# pd.options.mode.chained_assignment = None
 
-#Data Collection and EDA
-cars=pd.read_csv('Toyota.csv',index_col=0,na_values=['??',"????"])
-cars['Age'].fillna(cars['Age'].mean(),inplace=True)
-cars['KM'].fillna(cars['KM'].mean(),inplace=True)
-cars['FuelType'].fillna(cars['FuelType'].mode()[0],inplace=True)
-cars['HP'].fillna(cars['HP'].mean(),inplace=True)
-cars.drop('MetColor',axis=1,inplace=True)
-
-price=np.array(cars['Price'])
-
-#Data Preprocessing
 def featurescaling(arr):
     return (arr-np.min(arr))/(np.max(arr)-np.min(arr))
 
-n=price.shape[0]
+def encoding(arr):
+    j=0
+    for i in arr:
+        if i=="Petrol":
+            arr[j]=4
+        elif i=="Diesel":
+            arr[j]=3
+        elif i=="CNG":
+            arr[j]=2
+        else:
+            arr[j]=1
+        j+=1
+    return featurescaling(arr)
 
-age=np.array(cars['Age'])
-age=featurescaling(age)
+#Data Collection and Cleaning
+df=pd.read_csv('Toyota.csv',index_col=0,na_values=['??',"????"])
+df['Age'].fillna(df['Age'].mean(),inplace=True)
+df['KM'].fillna(df['KM'].mean(),inplace=True)
+df['FuelType'].fillna(df['FuelType'].mode()[0],inplace=True)
+df['HP'].fillna(df['HP'].mean(),inplace=True)
+df.drop('MetColor',axis=1,inplace=True)
+df.drop('Doors',axis=1,inplace=True)
 
-distance=np.array(cars['KM'])
-distance=featurescaling(distance)
+df['Age']=featurescaling(df['Age'])
+df['KM']=featurescaling(df['KM'])
+df['FuelType']=encoding(df['FuelType'])
+df['HP']=featurescaling(df['HP'])
+df['CC']=featurescaling(df['CC'])
+df['Weight']=featurescaling(df['Weight'])
 
-fueltype=np.array(cars['FuelType'])
-#Encoding Fuel Type using Label Encoder
-j=0
-for i in fueltype:
-    if i=="Petrol":
-        fueltype[j]=4
-    elif i=="Diesel":
-        fueltype[j]=3
-    elif i=="CNG":
-        fueltype[j]=2
-    else:
-        fueltype[j]=1
-    j+=1
-fueltype=featurescaling(fueltype)
+# sns.heatmap(df.corr(),fmt='.2f',annot=True)
+# plt.show()
 
-hp=np.array(cars["HP"])
-hp=featurescaling(hp)
+X=df.drop('Price',axis=1)
+y=df['Price']
 
-engtype=np.array(cars["Automatic"])
-engtype=featurescaling(engtype)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
 
-cc=np.array(cars["CC"])
-cc=featurescaling(cc)
+lr = LinearRegression()
+lr.fit(X_train, y_train)
 
-
-weight=np.array(cars["Weight"])
-weight=featurescaling(weight)
-
-lbd=0.2
-
-X=np.array([[1,age[k],distance[k],fueltype[k],hp[k],engtype[k],cc[k],weight[k]] for k in range(n)])
-theta=np.dot(np.linalg.inv(np.dot(X.T,X)+lbd*np.identity(8)),np.dot(X.T,price))
-
-theta_reshaped=np.reshape(theta,(8,1))
-print(f"Weights:{theta_reshaped}")
-predictions,actual,mse,error,incorrect,correct=[],[],[],[],0,0
-
-for i in range(n):
-    Y=np.dot(theta.T,np.array([[1],[age[i]],[distance[i]],[fueltype[i]],[hp[i]],[engtype[i]],[cc[i]],[weight[i]]]))
-    
-    mse.append((Y-price[i])**2)
-    error.append(abs(Y-price[i]))
-    if abs(Y-price[i])<=(price[i]*0.2):
-        correct+=1
-    else:
-        incorrect+=1
-    predictions.append(Y)
-    actual.append(price[i])
-    
-rmse=np.sqrt(np.mean(mse))
-mae=np.mean(error)
-
-print(f"Correct predictions: {correct} Incorrect predictions: {incorrect}")
-print(f"Accuracy: {correct/(correct+incorrect)*100}%")
-print(f"Root Mean Squared Error is {rmse}")
-print(f"Mean Absolute Error is {mae}")
-
-plt.scatter(actual,predictions,c='red')
-plt.xlabel("Actual Price")
-plt.ylabel("Predicted Price")
-plt.title("Predictions vs Actual")
-
-x1,y1=[0,25000],[0,25000]
-plt.plot(x1,y1,marker='o',c='green')
-plt.show()
+print('Linear Regression Train Score is : ' , lr.score(X_train, y_train))
+print('Linear Regression Test Score is : ' , lr.score(X_test, y_test))
+print('Linear Regression Coef is : ' , lr.coef_)
+print('Linear Regression intercept is : ' , lr.intercept_)
