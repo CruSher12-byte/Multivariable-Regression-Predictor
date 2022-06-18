4fae50a009d12cb91283a23a82860895519f35a6
Rishi More <rishimore102@gmail.com>
2022-06-18 18:15:47 +0530

    Updated Model
    Added Random Forest and Gradient Boosting Regression

--- RegressionModel.py
+++ RegressionModel.py
@@ -2,8 +2,10 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression,Ridge,Lasso
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
 #Data Collection and Cleaning
 df=pd.read_csv('CarSales.csv',index_col=0,na_values=['??',"????"])
@@ -57,14 +59,26 @@ ls.fit(X_train,y_train)
 print('\nLasso Regression Train Score is : ' , ls.score(X_train, y_train))
 print('Lasso Regression Test Score is : ' , ls.score(X_test, y_test))
 
-dt=DecisionTreeRegressor(max_depth=8)
-dt.fit(X_train,y_train)
-
-print("\nDecision Tree Regression Train Score is : " , dt.score(X_train, y_train))
-print("Decision Tree Regression Test Score is : " , dt.score(X_test, y_test))
-
 knn=KNeighborsRegressor(n_neighbors=8)
 knn.fit(X_train,y_train)
 
 print("\nKNN Regression Train Score is : " , knn.score(X_train, y_train))
 print("KNN Regression Test Score is : " , knn.score(X_test, y_test))
+
+dt=DecisionTreeRegressor(max_depth=10)
+dt.fit(X_train,y_train)
+
+print("\nDecision Tree Regression Train Score is : " , dt.score(X_train, y_train))
+print("Decision Tree Regression Test Score is : " , dt.score(X_test, y_test))
+
+forest=RandomForestRegressor(criterion='absolute_error',n_estimators=500,max_depth=10,random_state=0,n_jobs=-1)
+forest.fit(X_train,y_train)
+
+print("\nRandom Forest Regression Train Score is : " , forest.score(X_train, y_train))
+print("Random Forest Regression Test Score is : " , forest.score(X_test, y_test))
+
+gbr=GradientBoostingRegressor(random_state=0,n_estimators=20,max_depth=8)
+gbr.fit(X_train,y_train)
+
+print("\nGradient Boosting Regression Train Score is : " , gbr.score(X_train, y_train))
+print("Gradient Boosting Regression Test Score is : " , gbr.score(X_test, y_test))
